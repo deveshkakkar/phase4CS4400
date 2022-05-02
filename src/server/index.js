6fe36ac52273c7287f7314afdcac97c1eeb47541
kdadhaniya <kdadhaniya3@gatech.edu>
2022-05-01 22:55:12 -0400

    adding sign in screen

--- src/server/index.js
+++ src/server/index.js
@@ -16,6 +16,57 @@ app.get("/api/get", (req,res)=>{
     res.send(result)
     });   });
 
+app.post("/api/loginUsers", (req,res)=>{
+    console.log(req.body);
+    const userID = req.body.userID;
+    const pw = req.body.pw;
+    var listOfUsers = []; 
+    var listOfAdmins = []; 
+    var isInSystem = false;
+    console.log(userID);
+    console.log(pw);
+    db.query("SELECT perID, pwd FROM person", (err,result)=>{
+        if(err) {
+            console.log(err)
+        } 
+        listOfUsers = result;
+        console.log(listOfUsers)
+        for (let i = 0; i < listOfUsers.length; i++) {
+            if( userID == listOfUsers[i]['perID'] && pw == listOfUsers[i]['pwd']) {
+                console.log("In the system");
+                isInSystem = true;
+                break;
+            }
+        }
+        if (!isInSystem) {
+            console.log(" Not In the system");
+            res.send("Not found");
+        } else {
+            //check for admin
+            db.query("SELECT perID FROM system_admin",  (err,result) => {
+                listOfAdmins = result;
+                if(err) {
+                    console.log(err)
+                } 
+                for (let i = 0; i < listOfAdmins.length; i++) {
+                    if( userID == listOfAdmins[i]['perID']) {
+                        console.log("In the admin system");
+                        adminIsInSystem = true;
+                        break;
+                    }
+                }
+                if (adminIsInSystem) {
+                    res.send("Admin");
+                }
+
+
+            });
+
+        }
+        });  
+    
+ });
+    
 
 app.post('/api/create', (req,res)=> {
 
